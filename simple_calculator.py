20c688f370f4dcc83f059a102e324cb747a7ffa0
UTKARSH TIWARI <utkarshtiwari2803@gmail.com>
2022-03-16 22:59:11 +0530

    Added features to handle double digit numbers and multiple numbers

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,15 +1,24 @@
 class SimpleCalculator:
     def __init__(self) -> None:
         self.inputStr = ""
+        self.totalSum = 0
     
+    # Method to add 2 numbers and return it's sum value
     def Add(self, inputString):
         self.inputStr = inputString
 
+        print(self.inputStr)
         if self.inputStr == "":
             return 0
              
-        if len(self.inputStr) == 1:
+        if "," not in self.inputStr:
             return int(self.inputStr)
         else:
             operands = self.inputStr.split(",")
-            return int(operands[0])+int(operands[1])
+            for i in range(len(operands)):
+                self.totalSum += int(operands[i])
+            return self.totalSum
+
+# inputNumber = input("enter input:")
+# calculator = SimpleCalculator()
+# print("TotalSum ---> {}".format(calculator.Add(inputNumber)))
--- test_simple_calculator.py
+++ test_simple_calculator.py
@@ -5,22 +5,25 @@ class TestAddition(unittest.TestCase):
 
     def setUp(self):
         self.calculator = SimpleCalculator()
-        pass
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
   
     # Returns True if the add method returns 0 for an empty string.
     def test_strings_empty(self):
         self.assertEqual(self.calculator.Add(""), 0)
   
     # Returns True if the add method returns number for a stirng wtih one number.
-    def test_strings_with_one_number(self):      
+    def test_with_one_number_single_dgit(self):      
         self.assertEqual(self.calculator.Add("1"), 1)
+    
+    def test_with_one_number_multiple_digits(self):
         self.assertEqual(self.calculator.Add("12"), 12)
     
-    def test_strings_with_two_numbers(self):
+    def test_with_two_numbers(self):
         self.assertEqual(self.calculator.Add("2,3"), 5)
 
-    def test_strings_with_more_than_two_numbers(self):
-        self.assertEqual(self.calculator.Add("2,3,4"), 9)
+    def test_with_more_than_two_numbers(self):
         self.assertEqual(self.calculator.Add("20,11,13"), 44)
   
 if __name__ == '__main__':
